57283b2ff9d8d2e2ce6ff10c16e46305f34cee88
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-11-16 01:51:24 +0200

    fix redux

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,42 +1,70 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { addContact, deleteContact, fetchContacts } from 'redux/operations';
+
+const handlePanding = state => {
+  state.contacts.isLoading = true;
+};
+
+const handleFulfilled = (state, action) => {
+  state.contacts.isLoading = false;
+  state.contacts.error = null;
+};
+
+const handleRejected = (state, action) => {
+  state.contacts.isLoading = false;
+  state.contacts.error = action.payload;
+};
 
 const contactSlice = createSlice({
-  // Ім'я слайсу
   name: 'contacts',
-  // Початковий стан редюсера слайсу
   initialState: {
-    contacts: [],
+    contacts: {
+      items: [],
+      isLoading: false,
+      error: null,
+    },
   },
-  // Об'єкт редюсерів
-  // будемо перераховувати методи які в майбутньому будемо використовувати
-  reducers: {
-    addContacts: {
-      prepare: ({ name, number }) => {
-        return {
-          payload: {
-            id: nanoid(),
-            name,
-            number,
-          },
-        };
-      },
-      reducer: (state, action) => {
-        state.contacts.push(action.payload);
-      },
-    },
-    deleteContact(state, action) {
-      state.contacts = action.payload;
-    },
+
+  extraReducers: builder => {
+    builder
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.contacts.items.push(action.payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        const index = state.contacts.items.findIndex(
+          item => item.id === action.payload.id
+        );
+        state.contacts.items.splice(index, 1);
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.contacts.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, handleRejected) //для одного запиту
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.pending,
+          addContact.pending,
+          deleteContact.pending
+        ),
+        handlePanding
+      ) // якщо хочаб один запиn відбувається, то виконується
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.rejected,
+          addContact.rejected,
+          deleteContact.rejected
+        ),
+        handleRejected
+      )
+      .addMatcher(
+        isAnyOf(
+          addContact.fulfilled,
+          deleteContact.fulfilled,
+          fetchContacts.fulfilled
+        ),
+        handleFulfilled
+      );
   },
 });
 
-// Генератори екшенів
-//щоб запустити додавання треба створити екшн/ передати в UI/ викликати там екшн
-// і тоді редюсер зрозуміє що саме ця подія відбулась
-//звертаємось до нашого слайса і через крапку викликаємо екшини (створюються автоматично)
-export const { addContacts, deleteContact } = contactSlice.actions;
-
-// достаємо редюсер. саме його підключаємо в стор
-// contactSlice.reducer - кореневий редюсер - який формується із нашого reducers
 export const contactReducer = contactSlice.reducer;
